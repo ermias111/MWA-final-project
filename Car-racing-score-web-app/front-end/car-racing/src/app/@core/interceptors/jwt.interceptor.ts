7df01474436de6b69913365545107ab3937dab02
Sirak <sirak123hw@gmail.com>
2021-11-15 00:38:10 -0600

    add interceptors and utils

---
+++ Car-racing-score-web-app/front-end/car-racing/src/app/@core/interceptors/jwt.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+// import { environment } from '@env/environment';
+import { getItem, removeItem, setItem, StorageItem } from '../../@core/utils';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService){
+  }
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
+    const isLoggedIn = this.authService.isLoggedIn$.getValue();
+    const token = getItem(StorageItem.Auth);
+    // const isApiUrl = request.url.startsWith(environment.apiUrl);
+    
+    if (isLoggedIn) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
