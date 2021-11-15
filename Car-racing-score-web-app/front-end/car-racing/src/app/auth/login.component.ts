da76882bc2246c119dfa77e8bfe1c2bc388b5dbc
ermias111 <ermimichael@gmail.com>
2021-11-15 00:47:05 -0600

    add signup and login page using angular material

--- Car-racing-score-web-app/front-end/car-racing/src/app/auth/login.component.ts
+++ Car-racing-score-web-app/front-end/car-racing/src/app/auth/login.component.ts
@@ -1,16 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { loginI } from './dto/loginDto';
 
 @Component({
   selector: 'app-login',
   template: `
-    <p>
+    <!-- <p>
       <button [routerLink]="['/home/userhome']" (click)='login(loginData)'>login</button>
       login works!
-    </p>
+    </p> -->
+
+    <mat-card>
+        <mat-card-title>Login</mat-card-title>
+        <mat-card-content>
+          <form [formGroup]="form" (ngSubmit)="submit()">
+            <p>
+              <mat-form-field>
+                <input type="text" matInput placeholder="Username" formControlName="username">
+              </mat-form-field>
+            </p>
+
+            <p>
+              <mat-form-field>
+                <input type="password" matInput placeholder="Password" formControlName="password">
+              </mat-form-field>
+            </p>
+
+            <p *ngIf="error" class="error">
+              {{ error }}
+            </p>
+
+            <div class="button">
+              <button type="submit" mat-button>Login</button>
+            </div>
+
+          </form>
+        </mat-card-content>
+    </mat-card>
   `,
   styles: [
+    `
+      :host {
+        display: flex;
+        justify-content: center;
+        margin: 100px 0px;
+      }
+
+      .mat-form-field {
+        width: 100%;
+        min-width: 300px;
+      }
+
+      mat-card-title,
+      mat-card-content {
+        display: flex;
+        justify-content: center;
+      }
+
+      .error {
+        padding: 16px;
+        width: 300px;
+        color: white;
+        background-color: red;
+      }
+
+      .button {
+        display: flex;
+        justify-content: flex-end;
+      }
+
+    `
   ]
 })
 export class LoginComponent implements OnInit {
@@ -24,7 +84,23 @@ export class LoginComponent implements OnInit {
     userName: "kygo88",
     password: "12345"
   }
+  
   login(loginData: loginI){
     this.authService.login(loginData);
   }
+
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+      this.login(this.loginData)
+    }
+  }
+  @Input() error: string | null | undefined;
+
+  @Output() submitEM = new EventEmitter();
 }
