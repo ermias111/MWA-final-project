612e4f73468bb43bd15f46186872f2517c0a1652
ermias111 <ermimichael@gmail.com>
2021-11-15 21:49:10 -0600

    update on guard

--- Car-racing-score-web-app/front-end/car-racing/src/app/@core/guards/auth.guard.ts
+++ Car-racing-score-web-app/front-end/car-racing/src/app/@core/guards/auth.guard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
+import { getItem, removeItem, setItem, StorageItem } from '../../@core/utils';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +15,11 @@ export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     const isLoggedIn = this.authService.isLoggedIn$.getValue();
     
-    if (isLoggedIn) {
+    if (isLoggedIn || getItem(StorageItem.Auth)) {
       return true;
     }
     
-    this.router.navigate(['/home/welcome']);
+    this.router.navigate(['/auth/login']);
 
     return false;
   }
