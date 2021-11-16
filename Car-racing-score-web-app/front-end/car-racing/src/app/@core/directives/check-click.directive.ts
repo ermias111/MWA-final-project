ae1413d552185174b28b1111a55bc4cd5743cdeb
ermias111 <ermimichael@gmail.com>
2021-11-16 09:59:46 -0600

    add name in header

---
+++ Car-racing-score-web-app/front-end/car-racing/src/app/@core/directives/check-click.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[CheckClick]'
+})
+export class CheckClickDirective {
+
+  constructor() { }
+
+}
--- Car-racing-score-web-app/front-end/car-racing/src/app/home/header.component.ts
+++ Car-racing-score-web-app/front-end/car-racing/src/app/home/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getItem, StorageItem } from '../@core/utils';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -18,7 +19,7 @@ import { AuthService } from '../auth/auth.service';
           </button> -->
           
           <span class="example-spacer"></span>
-          
+          <span>{{firstName}} </span>
           <button mat-icon-button (click)='signout()' >
             <mat-icon>logout</mat-icon>
           </button>
@@ -36,6 +37,7 @@ import { AuthService } from '../auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
   isAdmin$: boolean = this.authService.isAdmin$.getValue();
+  firstName = getItem(StorageItem.FirstName);
 
   constructor(private authService: AuthService) { }
 
